c4b5a4a5a728581aaa685f995f15ca0030ce609d
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-10-03 05:26:08 +0700

    Updated UI and mobile responsiveness for HeroSection.

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -3,28 +3,27 @@ import { Vortex } from "../ui/vortex";
 
 const HeroSection = () => {
   return (
-    <div className="w-screen h-screen mx-auto rounded-md h-[30rem] overflow-hidden">
+    <div className="w-screen h-[95vh] md:h-screen mx-auto rounded-md h-[30rem] overflow-hidden">
       <Vortex
         rangeY={800}
         particleCount={550}
         baseHue={150}
-        className="flex items-center flex-col justify-center px-2 md:px-60 py-4 w-full h-full"
+        className="flex items-center flex-col justify-center px-8 md:px-60 py-4 w-full h-full"
       >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-          Elevate Your Trading Success
+        <h2 className="text-white text-2xl md:text-6xl md:max-w-4xl font-bold text-center">
+          Elevate Your Trading Success with BehindTrader
         </h2>
-        <h2 className="text-white text-2xl mt-2 md:text-6xl font-bold text-center">
-          with BehindTrader
-        </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          Take your trading to the next level together with us. Proven results,
-          trusted by experts.
+        <p className="text-white text-sm md:text-2xl md:max-w-2xl mt-6 text-center">
+          Take your trading to the next level. Copy our strategies. Trusted by
+          experts. Proven with results.
         </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
+        <div className="flex flex-row items-center gap-2 md:gap-4 mt-8">
+          <button className="px-4 py-2 text-sm md:text-lg bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
             Join Signal Room
           </button>
-          <button className="px-4 py-2  text-white ">Contact Us</button>
+          <button className="px-4 py-2 text-sm md:text-lg text-white ">
+            Contact Us
+          </button>
         </div>
       </Vortex>
     </div>
